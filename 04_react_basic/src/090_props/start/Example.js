2b08a2f46917e665b4a9dd5959595b4f6297fc5e
akachiryo <rirugameri@icloud.com>
2023-04-04 20:45:24 +0900

    props 練習

--- 04_react_basic/src/090_props/start/Example.js
+++ 04_react_basic/src/090_props/start/Example.js
@@ -4,9 +4,18 @@ const Example = () => {
   const hello = (arg) => {
     return `Hello ${arg}`
   }
+  const o = {
+    color: 'red',
+    num: 123
+  }
   return (
     <>
-      <Child num={123} />
+      <Child
+        {...o}
+        fn={hello}
+        bool
+        obj={{ name: 'Tom', age: 18 }}
+      />
       {/* <Child color="red"/> */}
     </>
   );
