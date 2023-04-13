50170cde3eff94ae8c8e54e7d55a7c2b53454ff2
akachiryo <rirugameri@icloud.com>
2023-04-13 22:20:51 +0900

    DOMそうさ

--- 06_control_and_form/src/120_select/start/Example.js
+++ 06_control_and_form/src/120_select/start/Example.js
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+
 const Example = () => {
+  const [selected, setSelected] = useState('Apple')
+  const OPTIONS = ['Apple', 'Banana', 'Cherry']
+
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      <select 
+        value={selected}
+        onChange={(e) => setSelected(e.target.value)}
+      >
+        {OPTIONS.map((val) => {
+          return (
+            <option key={val} value={val}>{val}</option>
+          )
+        })}
+      </select>
+      <p>選択された果物：{selected}</p>
+    </>
   );
 };
 
