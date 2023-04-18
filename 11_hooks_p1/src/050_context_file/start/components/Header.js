546851b518fa1c82b56a3acb6c21255094c5b802
akachiryo <rirugameri@icloud.com>
2023-04-18 23:30:07 +0900

    test

--- 11_hooks_p1/src/050_context_file/start/components/Header.js
+++ 11_hooks_p1/src/050_context_file/start/components/Header.js
@@ -1,5 +1,21 @@
+import { useTheme } from "../context/ThemeContext"
+
 const Header = () => {
-  
+  const THEMES = ['light', 'dark', 'red'];
+  const changeTheme = (e) => setTheme(e.target.value)
+  const [theme, setTheme] = useTheme();
+  return (
+    <header className={`content-${theme}`}>
+    {THEMES.map((_thema) => {
+      return (
+        <label>
+          <input type="radio" key={_thema} value={_thema} checked={theme === _thema } onChange={changeTheme}/>
+          {_thema}
+        </label>
+      )
+    })}
+    </header>
+  )
 };
 
 export default Header;
