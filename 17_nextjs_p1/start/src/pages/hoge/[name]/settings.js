2921c5cd93978b8ed13e49d785d100f5c8c51365
akachiryo <rirugameri@icloud.com>
2023-05-17 00:10:01 +0900

    useRouter push replace back reload

--- 17_nextjs_p1/start/src/pages/hoge/[name]/settings.js
+++ 17_nextjs_p1/start/src/pages/hoge/[name]/settings.js
@@ -2,6 +2,14 @@ import { useRouter } from "next/router";
 
 export default function Settings({ query }) {
   const router = useRouter()
+  const clickHandler = () => {
+    router.reload()
+  }
   console.log(router)
-  return <h1>routerから取得：{ router.query.name }</h1>
+  return (
+    <>
+      <h1>routerから取得：{ router.query.key }</h1>
+      <button onClick={clickHandler}>アクションによる画面遷移</button>
+    </>
+  )
 }
