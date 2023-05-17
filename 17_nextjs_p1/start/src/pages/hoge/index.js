a56c5403f65df6be0e6ad52e97df593ab486186c
akachiryo <rirugameri@icloud.com>
2023-05-18 00:09:38 +0900

    linkタグ　使い方習得

--- 17_nextjs_p1/start/src/pages/hoge/index.js
+++ 17_nextjs_p1/start/src/pages/hoge/index.js
@@ -1,3 +1,12 @@
+import Link from 'next/link'
+
 export default function Hoge() {
-  return <h1>Hoge</h1>
+  return (
+    <>
+      <h1>hoge</h1>
+      <Link href={{pathname: "/hoge/blog/first", query: {ley: "value"}}}>
+        <a>o7_router</a>
+      </Link>
+    </>
+  )
 }
